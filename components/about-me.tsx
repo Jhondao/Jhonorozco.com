b9735775fd5fbb687371994e977e2f6f0624fd01
Jhon Orozco <jhondao.desarrollo@hotmaile.com>
2024-08-28 21:49:27 -0500

    [FIX] Section the about me and add section the work

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -1,18 +1,38 @@
 import React from "react";
 import Title from "./shared/title";
-import { dataAboutMe } from "@/data";
+import { dataAboutMe, dataSlider } from "@/data";
 import { Button } from "./ui/button";
 import { Phone } from "lucide-react";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel";
+import Image from "next/image";
 
 export default function AboutMe() {
   return (
     <div className="p-6 md:px-12 md:py-30 max-w-5xl mx-auto" id="about-me">
       <Title title="Sobre mí" subtitle="Conóceme" />
 
-      <div className="frid md:grid-cols-2">
+      <div className="grid md:grid-cols-2">
         <div className="py-12 md_py-9 flex items-center justify-center">
-          {/* CAROUSEL */}
+          <Carousel  opts={{
+              align: "start"
+            }}
+            orientation="vertical"
+            className="w-full max-w-xs h-fit">
+
+           <CarouselContent className="-m-1 h-[200px]">
+            {dataSlider.map((data) => (
+              <CarouselItem key={data.id}>
+                <div className="flex items-center justify-center">
+                  <Image src={data.url} alt="Image" width={250} height={400} className="w-full h-auto rounded-lg"/>
+                </div>
+              </CarouselItem>
+            ))}
+           </CarouselContent>
+           <CarouselPrevious/>
+           <CarouselNext />
+          </Carousel>
         </div>
+        <div>
         <div className="grid md:grid-cols-3 mt-7 gap-4">
           {dataAboutMe.map((data) => (
             <div
@@ -43,6 +63,7 @@ export default function AboutMe() {
             <Button>
               <Phone size={20} className="mr-2" /> Hablamos
             </Button>
+        </div>
         </div>
       </div>
     </div>
---
+++ components/testimonials.tsx
@@ -0,0 +1,11 @@
+import Title from "./shared/title";
+
+const Testimonials = () => {
+    return ( 
+        <div className="p-4 md:px-12 md:py-44 max-w-5xl mx-auto">
+            <Title title="Testimonios" subtitle="Review de los clientes" />
+        </div>
+     );
+}
+ 
+export default Testimonials;
