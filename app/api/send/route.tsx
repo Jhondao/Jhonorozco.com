6c9f85fdfe2f838ef5d0981d9533ac47516a774c
Jhon Orozco <jhondao.desarrollo@hotmaile.com>
2024-09-15 14:17:56 -0500

    [ADD] Space for the form in the site

---
+++ app/api/send/route.tsx
@@ -0,0 +1,29 @@
+
+import { Resend } from "resend";
+import { EmailTemplate } from "@/components/email-template";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(req: Request) {
+  try {
+    const dataForm = await req.json();
+    try {
+      const data = await resend.emails.send({
+        from: "Acme <onboarding@resend.dev>",
+        to: ["jhondao.desarrollo@hotmail.com"],
+        subject: "Portafolio JhonOrozco.com",
+        react: EmailTemplate({
+          firstName: dataForm.username,
+          message: dataForm.message,
+          email: dataForm.email,
+        }),
+        text: "JhonOrozco",
+      });
+      return Response.json(data);
+    } catch (error) {
+      return Response.json({ error });
+    }
+  } catch (error) {
+    return Response.json({ error });
+  }
+}
